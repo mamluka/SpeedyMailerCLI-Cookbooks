dd2c0bc38a64117774d6a5c6982570e41df6b38a
SpeedyMailer Server <davidmz@mail.domain.com>
2013-09-07 04:38:56 -0700

    update setup even more

--- setup.rb
+++ setup.rb
@@ -1,17 +1,17 @@
 require 'fileutils'
 require 'json'
 
-text = File.read('solo.rb')
-File.open('solo.rb','w') { |f| f.puts text.gsub(/homedir/,ENV['HOME'])}
+FileUtils.cp('solo.rb','../')
+FileUtils.cp('node.json','../')
 
-node = JSON.parse(File.read('node.json'))
+text = File.read('../solo.rb')
+File.open('../solo.rb','w') { |f| f.puts text.gsub(/homedir/,ENV['HOME'])}
+
+node = JSON.parse(File.read('../node.json'))
 
 node['drone'] = Hash.new
 node['drone']['domain'] = ARGV[0]
 node['drone']['ip'] = `wget -q -O- http://ipecho.net/plain`
+node['drone']['master'] = ARGV[1]
 
-File.open('node.json','w') { |f| f.puts JSON.generate(node) }
-
-
-FileUtils.cp('solo.rb','../')
-FileUtils.cp('node.json','../')
+File.open('../node.json','w') { |f| f.puts JSON.generate(node) }
